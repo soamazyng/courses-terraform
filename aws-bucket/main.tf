7533906809ae1753c13fb529dcb3cb6b51c9fa27
Jaqueline Benedicto <soamazing@gmail.com>
2022-11-07 13:35:00 -0300

    +

---
+++ aws-bucket/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      version = "3.73.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      owner      = "Jaque"
+      managed-by = "terraform"
+    }
+  }
+}
